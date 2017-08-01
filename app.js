ae2a2abb5e11a93c9d7df24f4cb6467490c813c1
rkiang <ryanlkiang@gmail.com>
2017-08-01 16:25:13 -0500

    added app.use for public folder

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var app = express();
 
 var port = 3000;
 
-app.get('/', function (req, res) {
-    res.send('Testing');
-})
+// adding public folder to app.js
+app.use(express.static('public'))
+
+// app.get('/', function (req, res) {
+//     res.send('Testing');
+// })
 
 app.listen(port, function() {
     console.log('Listening to port', port);  
